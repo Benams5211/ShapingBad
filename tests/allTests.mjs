b516736fc7d108a6538af32dfce0aaa68e7f49f3
A-I-Hall <ahall142@kent.edu>
2025-12-07 06:10:12 -0500

    Update allTests.mjs

--- tests/allTests.mjs
+++ tests/allTests.mjs
@@ -2,7 +2,7 @@
 import './clickArea-test.mjs';
 import './varSetting-test.mjs';
 import './localstorageManager-test.mjs'; 
-import './teleport-test.mjs';
+
 
 import { runner } from './testClass.js';
 
